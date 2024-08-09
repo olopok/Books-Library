f29c65a2b7f5ca25f36f04e781beb5215ab2317b
olopok <pabisa.pb@gmail.com>
2024-08-09 20:54:18 +0100

    Add a table to display the list. Add a modal displayed when 'Add' button been have been clicked. Some css and js.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+const myLibrary = [];
+const dialogElem = document.querySelector("dialog");
+const shwBtn = document.querySelector('.btn-show-modal');
+
+shwBtn.addEventListener('click', () => {
+    dialogElem.showModal();
+} )
+
+function Book (author, title, numPages, note) {
+    this.author = author;
+    this.title = title;
+    this.numPages = numPages;
+    this.note = note;
+}
+
 
