27a8aafe100892a47e522bff3b1e1a16f71dbccc
olopok <pabisa.pb@gmail.com>
2024-08-09 21:47:29 +0100

    Add close function.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 const myLibrary = [];
 const dialogElem = document.querySelector("dialog");
 const shwBtn = document.querySelector('.btn-show-modal');
+const clsBtn = document.querySelector('#close');
 
 shwBtn.addEventListener('click', () => {
     dialogElem.showModal();
-} )
+} );
+
+clsBtn.addEventListener('click', () => {
+    dialogElem.close();
+});
 
 function Book (author, title, numPages, note) {
     this.author = author;
