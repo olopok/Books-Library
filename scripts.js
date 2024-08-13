9978d3bf4715bb3a5e7265bf655c2285d6aa58c7
olopok <pabisa.pb@gmail.com>
2024-08-13 20:33:02 +0100

    Create routine to insert the elements in the table

--- scripts.js
+++ scripts.js
@@ -1,23 +1,18 @@
-const myLibrary = [];
-const dialogElem = document.querySelector("dialog");
-const shwBtn = document.querySelector('.btn-show-modal');
-const clsBtn = document.querySelector('#close');
-const addBtn = document.querySelector('#submit');
-
-const textAuthor = document.getElementById('author');
-
-// let myForm = document.querySelector('form');
+let library = [];
+const DIALOG_ELMENT = document.querySelector("dialog");
+const SHW_BTN = document.querySelector('.btn-show-modal');
+const CLS_BTN = document.querySelector('#close');
+const ADD_BTN = document.querySelector('#submit');
+const FORM = document.querySelector('form')
 
 
-shwBtn.addEventListener('click', () => {
-    dialogElem.showModal();
+SHW_BTN.addEventListener('click', () => {
+    DIALOG_ELMENT.showModal();
 });
 
-clsBtn.addEventListener('click', () => {
-    dialogElem.close();
+CLS_BTN.addEventListener('click', () => {
+    DIALOG_ELMENT.close();
 });
-
-addBtn.addEventListener('click', addBook, false);
 
 
 function Book(author, title, numPages, note) {
@@ -27,20 +22,61 @@ function Book(author, title, numPages, note) {
     this.note = note;
 }
 
+DIALOG_ELMENT.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const FORM_DATA = Object.fromEntries(new FormData(FORM));
+    const NEW_BOOK = new Book(FORM_DATA.author, FORM_DATA.title, FORM_DATA.pages, FORM_DATA.notes);
+    console.log(NEW_BOOK);
+    addBook(NEW_BOOK);
+    DIALOG_ELMENT.close();
+});
 
-function addBook(evt) {
-    const myForm = document.querySelector('form');
-    let formData = Object.fromEntries(new FormData(myForm));
-    const book = new Book(formData.author, formData.title, formData.pages, formData.notes);
-    myLibrary.push(book);
-    console.log(myLibrary);
-    // console.log(formData);
-    evt.preventDefault();
-    dialogElem.close();
-    return;
+function addBook(bookToAdd) {
+    library.push(bookToAdd);
+
+    const TABLE = document.querySelector('tbody');
+    const ROW = document.createElement('tr');
+    const READ_BUTTON = document.createElement('button');
+    const DEL_BTN = document.createElement('button');
+
+    DEL_BTN.className = "button";
+    DEL_BTN.textContent = "Delete";
+
+
+    const newEl = Object.values(library[0]);
+    console.log(newEl);
+    for (let x in newEl) {
+        const TABLE_DATA = document.createElement('td');
+
+        if (newEl[x] === "read") {
+            READ_BUTTON.className = "button";
+            READ_BUTTON.id = "readButton";
+            READ_BUTTON.textContent = newEl[x];
+            TABLE_DATA.appendChild(READ_BUTTON);
+            ROW.appendChild(TABLE_DATA);
+
+        } else if (newEl[x] === "unread") {
+            READ_BUTTON.className = "button";
+            READ_BUTTON.id = "unReadButton";
+            READ_BUTTON.textContent = newEl[x];
+            TABLE_DATA.appendChild(READ_BUTTON);
+            ROW.appendChild(TABLE_DATA);
+
+        } else
+            TABLE_DATA.textContent = `${newEl[x]}`;
+        ROW.appendChild(TABLE_DATA);
+
+    }
+
+    const TABLE_DEL_BUTTON = document.createElement('td');
+    TABLE_DEL_BUTTON.appendChild(DEL_BTN);
+    ROW.appendChild(TABLE_DEL_BUTTON);
+
+    TABLE.appendChild(ROW);
+    library = [];
 }
 
-console.log(myLibrary);
+console.log(library); //debug only.
 
 
 // const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'nor read yet')
