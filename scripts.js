1d1286dba0f330d23d8ad1c58f8ea95da8f9a020
olopok <pabisa.pb@gmail.com>
2024-08-15 19:35:18 +0100

    Add some js in scripts.js. exit

--- scripts.js
+++ scripts.js
@@ -6,3 +6,28 @@ function Book(author, title, numPages, note) {
     this.numPages = numPages;
     this.note = note;
 }
+
+const DIALOG = document.querySelector('dialog');
+const SHOW_MODAL = document.querySelector('.btn-show-modal');
+
+SHOW_MODAL.addEventListener('click', () => {
+    DIALOG.showModal();
+});
+
+const CANCEL_BTN = document.querySelector('#close');
+
+CANCEL_BTN.addEventListener('click', () => {
+    DIALOG.close();
+});
+
+const ADD_BTN = document.querySelector('#submit');
+const FORM = document.querySelector('form');
+let infoBook = {};
+
+DIALOG.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const USER_INPUT = new FormData(FORM);
+    infoBook = Object.fromEntries(USER_INPUT);
+    console.log(infoBook);
+    DIALOG.close();
+})
